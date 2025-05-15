b42e7d6848b1214203252860e92467b2e81b97ad
AbhishekPuri-4259 <abhiii4259@gmail.com>
2025-05-15 11:30:53 +0530

    Create Hello_World.cpp

---
+++ Hello_World.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+int main()
+{
+	int num = 10;
+	
+	//<< --> insertion operator
+	//:: --> scope resolution operator
+	std::cout<<"Hello World"<<std::endl;
+	std::cout<<"Value :"<<num<<std::endl;
+	std::cout<<"Enter Value ::";
+	
+	//>> --> extraction operator
+	std::cin>>num;
+	std::cout<<"Value :"<<num<<std::endl;
+	
+	return 0;
+}
