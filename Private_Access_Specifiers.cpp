9dc6a61586d3eac667ab1651263028cf4ef6a5c6
AbhishekPuri-4259 <abhiii4259@gmail.com>
2025-05-15 12:04:00 +0530

    Create Private_Access_Specifiers.cpp

---
+++ Private_Access_Specifiers.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+using namespace std;
+
+class student
+{
+	//Data members
+	int id;
+	string name;
+	float per;
+	
+	//Member functions
+	void getDetails()
+	{
+		cout<<"Enter Id:";
+		cin>>id;
+		cout<<"Enter Name:";
+		cin>>name;
+		cout<<"Enter Percentage:";
+		cin>>per;
+	}
+	void display()
+	{
+		cout<<"	Name: "<<name<<endl<<"Id: "<<id<<endl<<"Percentage: "<<per<<endl;
+	}
+};
+
+int main()
+{
+	student stud1, stud2, stud3;
+	stud1.id = 20;
+	stud1.getDetails();
+	stud1.display();
+	
+	return 0;
+}
