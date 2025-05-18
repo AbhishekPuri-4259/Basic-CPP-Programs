ee6ce5bcf49938d5f542eadedc42fa887bf99595
Abhishek Puri <abhiii4259@gmail.com>
2025-05-18 20:15:59 +0530

    Create Class_Object.cpp

---
+++ Class_Object.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class car
+{
+	public:
+		
+	//Data members
+	string car_brand;
+	string car_type;
+	string car_color;
+	float car_mileage;
+	
+	//Member functions
+	void display()
+	{
+		cout<<endl<<"************************";
+		cout<<endl<<"Brand: "<<car_brand<<endl<<"Type: "<<car_type<<endl<<"Color: "<<car_color<<endl<<"Mileage: "<<car_mileage<<"kmpl"<<endl;
+		cout<<"************************";
+	}
+};
+
+int main()
+{
+	car xuv7OO, i20, slavia;
+	int opt;
+
+	cout<<"Display the details of"<<endl;
+	cout<<"1.XUV7OO"<<endl<<"2.i20"<<endl<<"3.Slavia"<<endl<<endl;
+
+	cin>>opt;
+
+	switch(opt)
+	{
+		case 1 : xuv7OO.car_brand = "Mahindra";
+			 xuv7OO.car_type = "SUV";
+			 xuv7OO.car_color = "Blue";
+			 xuv7OO.car_mileage = 16.57;
+
+			 xuv7OO.display();
+			 break;
+
+                case 2 : i20.car_brand = "Hyundai";
+                         i20.car_type = "Hatchback";
+                         i20.car_color = "Red";
+                         i20.car_mileage = 19.50;
+
+                         i20.display();
+                         break;
+
+                case 3 : slavia.car_brand = "Skoda";
+                         slavia.car_type = "Sedan";
+                         slavia.car_color = "White";
+                         slavia.car_mileage = 18.73;
+
+                         slavia.display();
+                         break;
+
+
+		default: cout<<"Invalid Option"<<endl;
+			 break;
+	}
+	
+	return 0;
+}
