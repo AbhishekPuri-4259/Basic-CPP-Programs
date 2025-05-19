52771600472919bd876d63844b82cb68623df02f
Abhishek Puri <abhiii4259@gmail.com>
2025-05-19 12:33:17 +0530

    Create 7_Constructor_Destructor.cpp

---
+++ 7_Constructor_Destructor.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Employee
+{
+        public:
+        int id;
+        char *name;
+
+        Employee()
+        {
+                id = 0;
+                name = new char[10];
+
+                cout << "Constructor called" << endl << endl;
+        }
+        ~Employee()
+        {
+                delete name;
+
+                cout <<"Destructor called" << endl;
+        }
+};
+
+int main()
+{
+        Employee emp1;
+        cout << "The ID is " << emp1.id << endl;
+        strcpy(emp1.name, "Tingu");
+        cout << "The Name is " << emp1.name << endl << endl;
+        return 0;
+}
