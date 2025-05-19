e34f58adf292b16e7c7b27bbc04fec00a29e64eb
Abhishek Puri <abhiii4259@gmail.com>
2025-05-19 13:50:40 +0530

    Create 10_copy_constructor.cpp

---
+++ 10_copy_constructor.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class car
+{
+        public:
+
+        string car_brand;
+        string car_type;
+        string car_color;
+        float car_mileage;
+
+        //Constructor
+        car(string cbrand, string ctype, string ccolor, float cmileage)
+        {
+                car_brand = cbrand;
+                car_type = ctype;
+                car_color = ccolor;
+                car_mileage = cmileage;
+        }
+
+        void display()
+        {
+                cout<<endl<<"************************";
+                cout<<endl<<"Brand: "<<car_brand<<endl<<"Type: "<<car_type<<endl<<"Color: "<<car_color<<endl<<"Mileage: "<<car_mileage<<"kmpl"<<endl;
+        }
+};
+
+int main()
+{
+        car customer1("Mahindra", "SUV", "Blue", 16.57);
+        car customer2("Hyundai", "Hatchback", "Red", 19.50);
+
+        customer1.display();
+        customer2.display();
+
+        car customer3 = customer2;
+        customer3.display();
+
+        return 0;
+}
