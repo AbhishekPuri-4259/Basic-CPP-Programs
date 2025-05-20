aaff24cc99bdf193a6d600bd02ea69111f116a5c
Abhishek Puri <abhiii4259@gmail.com>
2025-05-20 10:30:50 +0530

    Create Multilevel_Inheritance.cpp

---
+++ Multilevel_Inheritance.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+class line
+{
+	public:
+	int length;
+};
+
+class Rectangle : public line
+{
+	public:
+	int breadth;
+	float area;
+
+	void R_area(int l, int b)
+	{
+		area = l * b;
+	}
+
+	void print_area()
+	{
+		cout << " Area: " << area << endl;
+	}
+};
+
+class Cuboid : public Rectangle
+{
+	public:
+	int height;
+
+	void C_area(int l, int b, int h)
+        {
+                area = l * b * h;
+        }
+};
+
+int main()
+{
+	Rectangle R1;
+	R1.R_area(5, 4);
+	cout << "Rectangle";
+	R1.print_area();
+
+	Cuboid C1;
+	C1.C_area(5, 4, 3);
+	cout << "Cuboid";
+        C1.print_area();
+}
