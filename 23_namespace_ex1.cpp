c3266461c91c50b5d1a6be9ff9dbaf07786b7081
Abhishek Puri <abhiii4259@gmail.com>
2025-05-23 15:03:45 +0530

    Create 23_namespace_ex1.cpp

---
+++ 23_namespace_ex1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x = 10;
+	cout << x << endl;
+	double x = 15.5; // Not allowed to have the same name in a local space!
+	cout << x << endl;
+	return 0;
+}
