f8fdc0213dafeac68e0f075b0613782f02d9d8bb
Abhishek Puri <abhiii4259@gmail.com>
2025-05-24 12:40:40 +0530

    Create 30_exception_handling_ex4_Bank_Withdrawal_System.cpp

---
+++ 30_exception_handling_ex4_Bank_Withdrawal_System.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+class HDFCBankAccount
+{
+	private:
+    	double balance;
+
+	public:
+    	HDFCBankAccount(double initialBalance)
+	{
+		balance = initialBalance;
+	}
+
+    	void withdraw(double w_amount) 
+	{       
+		try 
+		{
+            		if (w_amount > balance)
+			{
+                		throw "Insufficient balance for withdrawal";
+			}
+			else
+			{
+            			balance -= w_amount;
+            			cout << "Withdrawal successful!" << endl;
+				cout << "Remaining balance: " << balance << endl;
+			}
+        	}
+
+		catch (char const* error) 
+		{
+            		cout << error << endl;
+        	}
+    	}
+};
+
+int main() 
+{
+    HDFCBankAccount Harish(5000); //Initial balance = 5000
+
+    cout << "Attempting to withdraw 6000..." << endl;
+    Harish.withdraw(6000); // Exceeds balance
+
+    cout << "************************************************************" << endl;
+    cout << "Try Again" << endl;
+    cout << "************************************************************" << endl;
+
+    cout << "Attempting to withdraw 3000..." << endl;
+    Harish.withdraw(3000); // Valid withdrawal
+
+    return 0;
+}
